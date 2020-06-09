ab5e3075c9f4ac9345161408a13c32cf072aca3b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-08 19:22:11 -0700

    feat: Create Player and Room classes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from textwrap import wrap
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"{self.name}\n{wrap(self.description)}"
+
+    def __repr__(self):
+        return f"{self.name}: [description={self.description}]"
