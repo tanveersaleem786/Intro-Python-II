1a31c96f8298ecda051571359f496d09c3514484
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-06-09 21:02:41 -0400

    Writing Player Class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+        
+    def __str__(self):
+        return f'self.name = {self.name} \nself.description = {self.description}'
+
+
+myPlayer = Player("Player A", "I am Player A")
+
+print(myPlayer)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+
+
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+    
+    def __str__(self):
+        return f'self.name = {self.name} \nself.description = {self.description} \nself.n_to = {self.n_to} \nself.s_to = {self.s_to} \nself.e_to = {self.e_to} \nself.w_to = {self.w_to}'
+
+
+myRoom = Room("Dinning Room", "Capacity with 8 people", "north", "south", "east", "west")
+
+print(myRoom)
