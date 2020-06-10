da4aef44a8ac3e3e2c234fdd0fa88d4d25f926f1
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-09 18:40:43 -0700

    feat: Complete MVP Day 01

--- src/adv.py
+++ src/adv.py
@@ -1,16 +1,55 @@
+from os import system, name
 from room import Room
 from player import Player
 
-# Helper functions
+cmd_lookup = {
+    "quit": None,
+    "q": None,
+    "n": Player.move,
+    "e": Player.move,
+    "s": Player.move,
+    "w": Player.move,
+}
 
 
-def isValidInput(input):
-    cmd_list = ["q", "n", "s", "e", "w"]
-    return any(cmd for cmd in cmd_list if cmd == input)
+# Helper Functions
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def is_valid_input(user_input: str):
+    cmd_list = cmd_lookup.keys()
+    return any(cmd for cmd in cmd_list if cmd == user_input)
+
+
+def parse(player: Player, user_input: str):
+    # execute the command
+    if cmd_lookup[user_input]:
+        cmd_lookup[user_input](player, user_input)
+
+
+def evaluate(player: Player, user_input: str):
+    is_game_over = False
+
+    if is_valid_input(user_input):
+        if (user_input == "q" or user_input == "quit"):
+            is_game_over = True
+
+        # print(f"-- command received: '{user_input}' --")
+        parse(player, user_input)
+    else:
+        print("I'm sorry, I don't understand what you mean")
+
+    return is_game_over
+
 
 # Declare all the rooms
-
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -32,7 +71,6 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -59,17 +97,15 @@ player = Player(room["outside"])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+is_game_over = False
 
-while True:
+while not(is_game_over):
     print("========== ========== ==========")
     print(str(player) + "\n")
-    player_cmd = input("What would you like to do (q to quit)? ").lower()
-    print("========== ========== ==========\n\n")
+    # Read
+    user_input = input("What would you like to do (q to quit)? ").lower()
 
-    if isValidInput(player_cmd):
-        if (player_cmd == "q"):
-            break
-
-        print("-- command received --")
-    else:
-        print("I'm sorry, I don't understand what you mean")
+    # Evaluate / Parse
+    clear()
+    is_game_over = evaluate(player, user_input)
+# Loop
--- src/player.py
+++ src/player.py
@@ -1,11 +1,29 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+
 class Player:
-    def __init__(self, current_room):
-        self.current_room = current_room
+    def __init__(self, current_room: Room):
+        self.current_room: Room = current_room
 
     def __str__(self):
         return str(self.current_room)
 
     def __repr__(self):
         return f"Player: [current_room={self.current_room}]"
+
+    def move(self, direction: str):
+        move_in: dict = {
+            "n": self.current_room.n_to,
+            "e": self.current_room.e_to,
+            "s": self.current_room.s_to,
+            "w": self.current_room.w_to,
+        }
+
+        new_room = move_in[direction]
+        if (new_room):
+            print(direction)
+            self.current_room = new_room
+        else:
+            print("Sorry, you can't go in that direction")
