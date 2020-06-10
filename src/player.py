0eb1ab3eed3e58f1f24036d2bd3983f8f1d180af
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-09 22:33:14 -0700

    feat: Give Player ability to take an item

--- src/player.py
+++ src/player.py
@@ -1,11 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 from room import Room
+from item import Item
 
 
 class Player:
     def __init__(self, current_room: Room):
         self.current_room: Room = current_room
+        self.inventory: list = []
 
     def __str__(self):
         return str(self.current_room)
@@ -13,7 +15,9 @@ class Player:
     def __repr__(self):
         return f"Player: [current_room={self.current_room}]"
 
-    def move(self, *direction):
+    def move(self, *command):
+        direction, obj_str = command
+
         move_in: dict = {
             "n": self.current_room.n_to,
             "e": self.current_room.e_to,
@@ -21,9 +25,21 @@ class Player:
             "w": self.current_room.w_to,
         }
 
-        new_room = move_in[direction[0]]
-        if (new_room):
-            print(direction[0])
+        new_room = move_in[direction]
+        if new_room:
+            print(direction)
             self.current_room = new_room
         else:
             print("Sorry, you can't go in that direction")
+
+    def take(self, *command):
+        cmd_str, item_name = command
+        item = self.current_room.find_item(item_name)
+
+        if item:
+            self.inventory.append(item)
+            self.current_room.remove_item(item)
+
+            print(f"You pickup the {item_name}")
+        else:
+            print(f"There is no {item_name} here!")
